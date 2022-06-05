9bb8a62e5d50413ad6d3ed6d4bd0e2ab833c8d85
Siberian <siberian@sibprojects.com>
2022-06-05 03:09:06 +0100

    Add files

--- src/AcmeVet/Scheduling/Domain/Appointment/Pet.php
+++ src/AcmeVet/Scheduling/Domain/Appointment/Pet.php
@@ -1,2 +1,37 @@
 <?php
 
+namespace AcmeVet\Scheduling\Domain\Appointment;
+
+class Pet
+{
+    private string $name;
+    private string $ownerName;
+    private string $contactNumber;
+
+    private function __construct(string $name, string $ownerName, string $contactNumber)
+    {
+        $this->name = $name;
+        $this->ownerName = $ownerName;
+        $this->contactNumber = $contactNumber;
+    }
+
+    public static function create(string $name, string $ownerName, string $contactNumber)
+    {
+        return new self($name, $ownerName, $contactNumber);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getOwnerName(): string
+    {
+        return $this->ownerName;
+    }
+
+    public function getContactNumber(): string
+    {
+        return $this->contactNumber;
+    }
+}
